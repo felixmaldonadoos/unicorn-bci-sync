d9ddb9228fdef81061193fe9f01520201b275e5f
felixmaldonado <75142964+felixmaldonadoos@users.noreply.github.com>
2022-03-07 00:02:02 -0500

    comments, bugs

--- blink.py
+++ blink.py
@@ -3,28 +3,30 @@
 # This runs a simple loop to test arduino function. 
 # returns delays. 
 
+
+# After FALLING EDGE, there will be a pause of OFFTIME (s) after the impulse with width ONTIME (s)
+
 from pyfirmata import Arduino
 import time
 import msvcrt
 import numpy as np
 
 
-def main(N_STIMS = 10, DURATION = 10, DC_RATIO = 0.5):
-    ONTIME = DC_RATIO
-    OFFTIME = 1 - ONTIME
+def main(PIN=13,N_STIMS = 3, ONTIME = 0.2,OFFTIME = 5 ):
+    PIN = 13
+    port = 'COM5'
     print('Looking for arduino...')
     BOOT = time.time()
-    pin = 13
-    port = 'COM5'
     run = True
     # board parameters
     board = Arduino(port)
 
     # set initial output to low 
-    board.digital[pin].write(0)
+    board.digital[PIN].write(0)
     DELAY = time.time()-BOOT
     print('Arduino connect delay (s): %0.6f'%DELAY)
-    print('Starting...\n')
+    print('Starting in 5 seconds...\n')
+    time.sleep(5)
 
     # parameters to save for output data
     STARTTIME = time.time()
@@ -34,29 +36,28 @@ def main(N_STIMS = 10, DURATION = 10, DC_RATIO = 0.5):
     COUNT = 0
     OUTPUT = []
     # sends stimuli and prints data while t < DURATION
-    while (RUNTIME < DURATION ):
+    while (COUNT <= N_STIMS ):
         UP = time.time()
-        board.digital[pin].write(1)
+        board.digital[PIN].write(1)
         time.sleep(ONTIME)
-        board.digital[pin].write(0)
+        board.digital[PIN].write(0)
         time.sleep(OFFTIME)
         RUNTIME = time.time() - STARTTIME
-        OUTPUT.append([(time.time()-STARTTIME),time.time() - UP - 1]) # real time sent, real delay
+        OUTPUT.append([(RUNTIME + DELAY),time.time() - UP - (ONTIME+OFFTIME)]) # real time sent, real delay
         COUNT += 1
-        print('Stim # %0.1f sent at: %0.6f, Pulse delay(s): %0.6f'%(COUNT,RUNTIME,(time.time() - UP - 1)))
+        print('Stim # %0.1f sent at: %0.6f, Pulse delay(s): %0.6f'%(COUNT,RUNTIME + DELAY,(time.time() - UP - (ONTIME+OFFTIME))))
     else:
         run = False
-        print('Average pulse delay (s): %0.6f'%(np.asarray(ARR_DELAY).mean()))
         print('\nENDED..\n')
         print(np.asarray(OUTPUT))
+        print('')
 
 
 
 if __name__ == '__main__':
-    print('\n===================================================')
-    print('BLINKING ARDUINO LED FROM PIN 13 ..')
-    print('Simple code to test Tobii synchronization.')
-    print('Default sends 10 pulses, T = 1s at 50% duty cycle.')
+    print('\n===================================================\n')
+    print('Simple code to test Tobii synchronization.\nIntented to compare time stimulus is sent and recieved.\n')
+    print('Defaults to:\n- PIN = 13\n- N_STIMS = 3\n- ONTIME = 0.2 s\n- OFFTIME = 5 s\n- T = 5.2 s')
     print('===================================================\n')
 
 
